4802906fa7344b85f72c8234ede094557a953580
masud rana <masudfcs1@gmail.com>
2023-08-31 14:34:05 +0600

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Navbar from "./Navbar";
 import BannerSection from "./components/BannerSection";
 import BlogSection from "./components/BlogSection";
 import Category from "./components/Category";
+import FeatureSection from "./components/FeatureSection";
 import FeatureSectionBreakfast from "./components/FeatureSectionBreakfast";
 import FeatureSectionFruits from "./components/FeatureSectionFruits";
+import Footer from "./components/Footer";
 import Newsleter from "./components/Newsleter";
 
 const App = () => {
@@ -18,6 +20,8 @@ const App = () => {
       <BannerSection />
       <BlogSection />
       <Newsleter />
+      <FeatureSection />
+      <Footer />
     </main>
   );
 };
